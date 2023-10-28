adf34e15a14077782f1b9239edd0864cc8865930
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-28 21:29:19 +0200

    Utworzenie klasy BookDto

---
+++ src/main/java/com/example/demo10/dto/BookDto.java
@@ -0,0 +1,14 @@
+package com.example.demo10.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class BookDto {
+private String title;
+private String author;
+private Integer publicationYear;
+}
