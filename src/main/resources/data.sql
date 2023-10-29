0cf0b50994dfdbcdc59ca0970ee7e180edf6a277
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-29 19:44:41 +0100

    Wypełnienie tabeli danymi

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO book_model(title, author, publication_year)
+VALUES ('JAVA', 'Richardson', 2022);
+
+INSERT INTO book_model(title, author, publication_year)
+VALUES ('C#', 'SDA', 2021);
+
+INSERT INTO book_model(title, author, publication_year)
+VALUES ('Python', 'John M', 2018);
+
+INSERT INTO book_model(title, author, publication_year)
+VALUES ('CSS', 'SDA', 2022);
